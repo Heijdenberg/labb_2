dcb2ee3b03d21406017bb06535021ea8a75b1a65
Heijdenberg <jensheijdenberg@hotmail.com>
2025-10-02 18:22:06 +0200

    LevelElement added

--- labb_2/Elements/LevelElement.cs
+++ labb_2/Elements/LevelElement.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace labb_2.Elements
+namespace labb_2.Elements;
+
+internal abstract class LevelElement
 {
-    internal class LevelElement
+    protected char Sprite { get; }
+    public Position Position { get; set; }
+    public ConsoleColor SpriteColor { get; }
+
+    protected LevelElement(char sprite, ConsoleColor spriteColor, int y, int x)
     {
+        Sprite = sprite;
+        SpriteColor = spriteColor;
+        Position = new Position(y, x);
     }
+
+    public void Draw() { }
 }
