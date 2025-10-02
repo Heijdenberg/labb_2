de532634b04fb8b206545abf18b71d0f9be7a7f3
Heijdenberg <jensheijdenberg@hotmail.com>
2025-10-02 18:21:42 +0200

    Enemy doone

--- labb_2/Elements/Enemy.cs
+++ labb_2/Elements/Enemy.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace labb_2.Elements
+namespace labb_2.Elements;
+
+internal abstract class Enemy: LevelElement
 {
-    internal class Enemy
+    protected string Name { get; }
+    protected int HitPoints { get; set; }
+    protected Dice AttackDice { get; }
+    protected Dice DefenceDice { get; }
+
+    public Enemy(string name, int hp, Dice attackDice, Dice defenceDice, char sprite, ConsoleColor color, int y, int x)
+        : base(sprite, color, y, x)
     {
+        Name = name;
+        HitPoints = hp;
+        AttackDice = attackDice;
+        DefenceDice = defenceDice;
     }
+    public abstract void Update();
 }
