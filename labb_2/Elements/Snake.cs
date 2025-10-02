608fa7fba79a82573ae9bf885ed215efa9054ab8
Heijdenberg <jensheijdenberg@hotmail.com>
2025-10-02 18:22:27 +0200

    snake added

--- labb_2/Elements/Snake.cs
+++ labb_2/Elements/Snake.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace labb_2.Elements
+namespace labb_2.Elements;
+
+internal class Snake : Enemy
 {
-    internal class Snake
+    public Snake(int y, int x)
+        : base(name: "Snake",
+            hp: 25,
+            attackDice: new Dice(3, 4, 2),
+            defenceDice: new Dice(1, 8, 5),
+            sprite: 's',
+            color: ConsoleColor.Green,
+            y, x)
+    { }
+    public override void Update()
     {
+
     }
 }
