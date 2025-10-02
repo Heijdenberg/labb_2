0564d883a45c0837147c22e6cb239121652646dc
Heijdenberg <jensheijdenberg@hotmail.com>
2025-10-02 18:23:35 +0200

    position done

--- labb_2/Position.cs
+++ labb_2/Position.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace labb_2
+namespace labb_2;
+
+internal struct Position
 {
-    internal class Position
+    public int Y { get; set; }
+    public int X { get; set; }
+
+    public Position(int y, int x)
     {
+        X = y;
+        Y = x;
     }
 }
