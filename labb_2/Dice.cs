dd8890ebdd2862a29bd3328d8ff4f5120d838e04
Heijdenberg <jensheijdenberg@hotmail.com>
2025-10-02 17:58:08 +0200

    Dice done

--- labb_2/Dice.cs
+++ labb_2/Dice.cs
@@ -4,9 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace labb_2
+namespace labb_2;
+
+internal class Dice
 {
-    internal class Dice
+    private static readonly Random random = new Random();
+
+    private int numberOfDice;
+    private int sidesPerDice;
+    private int modifier;
+
+    public Dice(int numberOfDice, int sidesPerDice, int modifier)
     {
+        this.numberOfDice = numberOfDice;
+        this.sidesPerDice = sidesPerDice;
+        this.modifier = modifier;
+    }
+
+    public int Throw()
+    {
+        int result = 0;
+
+        for (int i = 0; i < numberOfDice; i++)
+        {
+            result += random.Next(1, sidesPerDice+1);
+        }
+
+        result += modifier;
+
+        return result;
+    }
+
+    public override string ToString()
+    {
+        string diceNotation = $"{numberOfDice}D{sidesPerDice}";
+
+        if (modifier > 0)
+        {
+            diceNotation += $"+{modifier}";
+        }
+        else if (modifier < 0)
+        {
+            diceNotation += $"{modifier}";
+        }
+
+            return diceNotation;
     }
 }
