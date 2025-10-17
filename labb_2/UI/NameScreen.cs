57427d9616525dbc0d013d5ff5f95a8668fd5817
Heijdenberg <jensheijdenberg@hotmail.com>
2025-10-17 13:13:44 +0200

    improved NameScreen

--- labb_2/UI/NameScreen.cs
+++ labb_2/UI/NameScreen.cs
@@ -11,12 +11,7 @@ namespace labb_2.UI
     {
         public static string SetName(int hight, int width)
         {
-            Console.WriteLine($"╔{new string('═', width - 2)}╗");
-            for (int ii = 0; ii < hight-2; ii++)
-            {
-                Console.WriteLine($"║{new string(' ', width - 2)}║");
-            }
-            Console.WriteLine($"╚{new string('═', width - 2)}╝");
+            DrawBox(hight, width);
 
             Console.SetCursorPosition((width/2)-10, (hight/2));
             Console.Write("Name: ");
@@ -24,14 +19,25 @@ namespace labb_2.UI
             name = char.ToUpper(name[0])+ name.Substring(1);
 
             Console.Clear();
+            DrawBox(hight, width);
 
             Console.SetCursorPosition((width / 2) - 10, (hight / 2));
-            Console.WriteLine($"Welcome {name}");
+            Console.WriteLine($"Welcome {name}!!!");
             Thread.Sleep(1500);
 
             Console.Clear();
 
             return name;
+        }
+
+        private static void DrawBox(int hight, int width)
+        {
+            Console.WriteLine($"╔{new string('═', width - 2)}╗");
+            for (int ii = 0; ii < hight - 2; ii++)
+            {
+                Console.WriteLine($"║{new string(' ', width - 2)}║");
+            }
+            Console.WriteLine($"╚{new string('═', width - 2)}╝");
         }
     }
 }
